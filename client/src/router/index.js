21a07f1d3150be15e21cf637139ebf5496fab385
gefl24 <99234064+gefl24@users.noreply.github.com>
2026-01-28 17:01:53 +0800

    Update index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SearchView from '@/views/SearchView.vue'
 import DownloadView from '@/views/DownloadView.vue'
+import SourceView from '@/views/SourceView.vue'
+import PlaylistView from '@/views/PlaylistView.vue'
+import LeaderboardView from '@/views/LeaderboardView.vue'
+import SettingsView from '@/views/SettingsView.vue'
 
 const routes = [
   {
@@ -18,6 +22,30 @@ const routes = [
     name: 'Download',
     component: DownloadView,
     meta: { title: '下载管理' }
+  },
+  {
+    path: '/source',
+    name: 'Source',
+    component: SourceView,
+    meta: { title: '音源管理' }
+  },
+  {
+    path: '/playlist',
+    name: 'Playlist',
+    component: PlaylistView,
+    meta: { title: '歌单管理' }
+  },
+  {
+    path: '/leaderboard',
+    name: 'Leaderboard',
+    component: LeaderboardView,
+    meta: { title: '音乐榜单' }
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: SettingsView,
+    meta: { title: '系统设置' }
   }
 ]
 
